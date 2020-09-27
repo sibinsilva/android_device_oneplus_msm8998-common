b07139896b49f03bfbec29b08f3829b0ac5e9284
luk1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    msm8998-common: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444
    
    Conflicts:
    	proprietary-files.txt

--- extract-files.sh
+++ extract-files.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -54,41 +54,25 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-    product/lib64/libdpmframework.so | product/lib/libdpmframework.so )
+    product/lib64/libdpmframework.so )
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
     ;;
-        system_ext/etc/init/dpmd.rc)
-            sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | \
-        system_ext/etc/permissions/qcrilhook.xml | system_ext/etc/permissions/telephonyservice.xml )
-            sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
-            ;;
-        system_ext/lib64/libdpmframework.so )
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-            ;;
-        vendor/etc/permissions/com.fingerprints.extension.xml )
-            sed -i 's/\/system\/framework\//\/vendor\/framework\//g' "${2}"
-            ;;
-        vendor/lib64/hw/fingerprint.goodix.so | vendor/lib/hw/fingerprint.goodix.so )
-            sed -i 's|\x00goodix.fingerprint\x00|\x00fingerprint\x00\x00\x00\x00\x00\x00\x00\x00|' "${2}"
-            ;;
     esac
 }
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSP_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+    setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT" false "$CLEAN_VENDOR"
 
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
 
-COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+COMMON_BLOB_ROOT="$AOSP_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 
 #
 # Fix framework path
