59abc7bfc2921264693ef024789b58acf81add99
Bruno Martins <bgcngm@gmail.com>
2020-11-12 17:55:14 +0000

    msm8998-common: Drop ims-ext-common hack
    It is no longer needed after updating to an ims.apk compatible
    with Android 10.
    
    This reverts commit 616c33470b9534c3fc6b434fb73c6e250d1392d5
    
    Change-Id: I2bd91e8ee9f8e25bc40b017e9b1be3fb32fce900

--- common.mk
+++ common.mk
@@ -418,7 +418,6 @@ PRODUCT_PACKAGES += \
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
-    ims-ext-common_system \
     telephony-ext \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
@@ -427,7 +426,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # Seccomp policy
