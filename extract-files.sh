5bc0a39381bd50e55de036dfc98829784f59d96c
sibinsilva <sibinsilva.com>
2021-03-08 21:16:30 +0000

    Revert "msm8998-common: Use libcutils-v29.so for libdpmframework.so"
    This reverts commit 3a2db4c34bfaccdde88d6f103e59e5e8c6243c53.

--- extract-files.sh
+++ extract-files.sh
@@ -73,9 +73,6 @@ function blob_fixup() {
         vendor/lib64/hw/fingerprint.goodix.so | vendor/lib/hw/fingerprint.goodix.so )
             sed -i 's|\x00goodix.fingerprint\x00|\x00fingerprint\x00\x00\x00\x00\x00\x00\x00\x00|' "${2}"
             ;;
-    product/lib64/libdpmframework.so )
-        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-    ;;
     esac
 }
 
