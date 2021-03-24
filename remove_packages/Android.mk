da89772fe63c3ef806ac9c97954fecbe59bd9732
Maitreya Patni <56203318+Maitreya29@users.noreply.github.com>
2021-03-24 10:43:10 +0530

    fix the gei out of space error

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt FM2 CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt TipsPrebuilt Maps Drive GoogleTTS Music PrebuiltGmail NgaResources RecorderPrebuilt SafetyHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
