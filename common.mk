fd63b846288e805e24f344dcc8ecb382c66c24fe
Maitreya Patni <maitreyapatni30@gmail.com>
2021-03-24 03:25:48 +0000

    Miui cam?
    - I hate myself.
    
    Signed-off-by: Maitreya Patni <maitreyapatni30@gmail.com>

--- common.mk
+++ common.mk
@@ -28,6 +28,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Experimental
+# Try miuicam
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
