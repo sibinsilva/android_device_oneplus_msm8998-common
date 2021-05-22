d8855b3bece9a693c9b4bf9da209a3770db17bac
Maitreya29 <maitreyapatni30@gmail.com>
2021-05-22 14:04:03 +0530

    msm8998-common: Copy devicesettings init file rather than displaysettings.
    In commit fb9c2f9 we changed file names, this commit is a follow up to that.

--- common.mk
+++ common.mk
@@ -251,7 +251,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.target.displaymodes.rc:$(TARGET_COPY_OUT_VENDOR)/init.target.displaymodes.rc
+    $(LOCAL_PATH)/rootdir/etc/init.target.devicesettings.rc:$(TARGET_COPY_OUT_VENDOR)/init.target.devicesettings.rc
 
 # IFAA (Fingerprint support for Alipay)
 PRODUCT_PACKAGES += \
